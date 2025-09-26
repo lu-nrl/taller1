99fbb4ee280275166ba83b7bd5dbcc4ea48aea36
lu-nrl <lunanicolle31@gmail.com>
2025-09-26 12:59:42 -0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,22 @@
+/*let nota1 = parseInt (prompt ("Promedio de nota"));
+let nota2 = parseInt (prompt ("Promedio de nota"));
+let nota3= parseInt (prompt ("Promedio de nota"));
+
+let resultado = nota1 + nota2 + nota3
+
+console.log ("El resultado es", nota1,  " + " , nota2, " + " , nota3, "es:", resultado)
+
+let Promedio= resultado / 3
+
+console.log ("el promedio es", resultado, "/", 3, "es:", Promedio)*/
+
+/*let num1 = parseFloat (prompt ("Ingrese el cateto 1"))
+let num2 = parseFloat (prompt ("ingrese el cateto 2"))
+
+let hipotenusa = Math.sqrt ( (num1**2) + (num2**2));
+console.log("el resultado de la hipotenusa es: ", hipotenusa);*/
+
+let fahrenheit = parseFloat (prompt(" la temperatura en grados Fahrenheit:"));
+let celsius = (fahrenheit -32) / 1.8;
+console.log (fahrenheit + "°f equivalen a " + celsius + "°C");
+
